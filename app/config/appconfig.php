f9522ec51f5ab75fee584afa259318f547bd358a
teepeegirl <teju.prasad@gmail.com>
2011-01-14 16:41:26 -0800

    Add config files to git.

---
+++ app/config/appconfig.php
@@ -0,0 +1,8 @@
+<?php
+
+    $config['user']['STATUS_UNVERIFIED'] = 0;
+    $config['user']['STATUS_VERIFIED'] = 1;
+    $config['user']['STATUS_VERIFIED_AND_ACTIVATED'] = 2;
+    
+    $config['field']['COMMA_ENCODE'] = chr(31);
+?>
